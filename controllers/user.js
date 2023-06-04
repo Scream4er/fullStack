f912551e368da4c4687fd548bc8a7b7994b6df84
ScreamKO <Chaykov.Artem1@gmail.com>
2023-06-05 00:30:01 +0200

    create user flow, routes, middleware. Config token

--- controllers/user.js
+++ controllers/user.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken')
 const login = async (req, res) => {
     const { email, password } = req.body;
 
-    if (!email && !password) {
+    if (!email || !password) {
         return res.status(400).json({ message: 'Please fill all inputs'})
     }
 
@@ -16,24 +16,78 @@ const login = async (req, res) => {
     });
 
     const isPasswordCorrect = user && (await brypt.compare(password, user.password));
+    const secret = process.env.JWT_SECRET;
 
-    if (user && isPasswordCorrect) {
+    if (user && isPasswordCorrect && secret) {
         res.status(200).json({
             id: user.id,
             email: user.email,
             name: user.name,
+            token: jwt.sign({id: user.id}, secret, { expiresIn: '30d'})
         })
     } else {
         return res.status(400).json({ message: 'Uncorrected login or password' })
     }
 }
 
+/**
+ *
+ * @route POST /api/user/register
+ * @desc Register
+ * @access publick
+ */
+
 const register = async (req, res) => {
-    res.send('register');
+    const { email, name, password } = req.body;
+
+    if (!email || !password || !name) {
+        return res.status(400).json({ message: 'Please feel important inputs'})
+    }
+
+    const registeredUser = await prisma.user.findFirst({
+        where: {
+            email
+        }
+    });
+
+    if (registeredUser) {
+        return res.status(400).json({ message: 'This email is currently in use'})
+    }
+
+    const salt = await brypt.genSalt(10);
+    const hashedPassword = await brypt.hash(password, salt);
+
+    const user = await prisma.user.create({
+        data: {
+            email,
+            name,
+            password: hashedPassword
+        }
+    });
+
+    const secret = process.env.JWT_SECRET;
+
+    if (user && secret) {
+        res.status(201).json({
+            id: user.id,
+            email: user.email,
+            name,
+            token: jwt.sign({id: user.id}, secret, { expiresIn: '30d'})
+        })
+    } else {
+        return res.status(400).json({ message: 'Cant create a user'})
+    }
 }
 
+/**
+ *
+ * @route GET /api/user/current
+ * @desc current user
+ * @access Private
+ */
+
 const current = async (req, res) => {
-    res.send('current');
+    return res.status(200).json(req.user);
 }
 
 module.exports = {
